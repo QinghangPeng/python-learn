2ee03ba8344885def364365899fd1a6390906876
JackSon <voyagejk.peng@gmail.com>
2019-06-27 23:18:20 +0800

    :sparkles: helloworld程序

---
+++ helloWorld.py
@@ -0,0 +1,8 @@
+print('============== this is a first python exe ===============')
+temp = input("你猜我现在心里想的数字是多少:")
+guess = int(temp)
+if guess == 6:
+    print("66666666")
+else:
+    print("果然我们没有心灵感应")
+print("this exe over")
