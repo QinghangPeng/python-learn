d463f4254e4bc0d727967d9b668628878c31a185
stealhao <penghao@zhiyuanhui.com>
2020-01-08 11:33:17 +0800

    excel处理

--- excel/excel.py
+++ excel/excel.py
@@ -1,9 +1,9 @@
 import openpyxl
 
 # 需要读取的文件
-book_name_xls = '/home/stealhao/下载/公安部/公交站点、线路/ÏßÂ·ÐÅÏ¢±í20200106.xlsx'
+book_name_xls = '/home/stealhao/下载/公安部/公交站点、线路/2019年五大公司配车生产计划表（七月）.xlsx'
 
-sheet_name_xlsx = 'Sheet1'
+sheet_name_xlsx = '7月'
 
 # 需要写出的文件
 product_name_xls = '/home/stealhao/下载/公安部/静态数据/公交/公交线路信息采集.xlsx'
@@ -13,13 +13,26 @@ def read_excel_xlsx(path, sheet_name):
     workbook = openpyxl.load_workbook(path)
     sheet = workbook[sheet_name]
     b = []
+    k = 1
     for row in sheet.rows:
-        a = []
-        for cell in row:
-            if cell.column_letter  in ['C','F','G']:
-                a.append(cell.value)
-        b.append(a)
+        print("当前读取第",k, "行")
+        if k > 5:
+            a = []
+            for cell in row:
+                # print("当前读取第",k,"行，第",cell.column_letter ,"列")
+                # print("当前读取第",k,"行，第",cell.coordinate ,"列")
+                c = build_cell_column(k)
+                if cell.coordinate  in c:
+                    d = cell.value
+                    a.append(d)
+            b.append(a)
+        k += 1
     write_excel_xlsx(product_name_xls,sheet_name_xlsx,b)
+
+def build_cell_column(index):
+    str_index = str(index)
+    a = ['B' + str_index,'EZ' + str_index,'FA' + str_index,'FB' + str_index,'FC' + str_index,'FD' + str_index,'FE' + str_index]
+    return a
 
 # 写入excel
 def write_excel_xlsx(path, sheet_name, value):
